c0896f46935f90be6708ffe0c7e1b74eff7df393
Miro <167845049+mirojones@users.noreply.github.com>
2025-11-13 10:13:50 +1100

    Update index.js

--- sphere/index.js
+++ sphere/index.js
@@ -1,8 +1,16 @@
-const csv = document.querySelector('pre');
+const csv = document.querySelector('pre'),
+  start = document.getElementById('start'),
+  stop = document.getElementById('stop'),
+  log = ev => csv.textContent += [ Date.now(), ev.alpha, ev.beta, ev.gamma ] + '\n';
 
-addEventListener('deviceorientation', ev => csv.textContent += [
-  Date.now(),
-  ev.alpha,
-  ev.beta,
-  ev.gamma 
-] + '\n');
+start.addEventListener('click', () => {
+  start.disabled = '';
+  stop.removeAttribute('disabled');
+  addEventListener('deviceorientation', log);
+});
+
+stop.addEventListener('click', () => {
+  start.removeAttribute('disabled');
+  stop.disabled = '';
+  removeEventListener('deviceorientation', log);
+});
