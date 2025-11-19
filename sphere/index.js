1404ee607ac97db459a51f6f1f4dcae5e0f5e50c
Miro <167845049+mirojones@users.noreply.github.com>
2025-11-20 09:24:52 +1100

    Update index.js

--- sphere/index.js
+++ sphere/index.js
@@ -1,7 +1,7 @@
 const csv = document.querySelector('pre'),
   start = document.getElementById('start'),
   stop = document.getElementById('stop'),
-  log = ev => csv.textContent = [ Date.now(), ev.alpha, ev.beta, ev.gamma ] + '\n' + csv.textContent;
+  log = ev => csv.textContent = [ Date.now(), ev.alpha, ev.beta, ev.gamma ];
 
 start.addEventListener('click', () => {
   start.setAttribute('disabled', '');
